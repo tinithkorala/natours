24bdd26753948442161387017d61c2c0d7c4e2bd
Tinithk <tinithse@gmail.com>
2023-04-17 23:01:12 +0530

    model and schema created

--- server.js
+++ server.js
@@ -14,6 +14,26 @@ mongoose.connect(DB, {
   useNewUrlParser: true
 }).then(() => console.log('DB connection successful !'));
 
+// Schema
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    require: [true, 'A tour must have a price']
+  }
+});
+
+// Create a model from the schema
+const Tour = mongoose.model('Tour', tourSchema);
+
 // console.log(process.env);
 
 const port = process.env.PORT || 3000;
