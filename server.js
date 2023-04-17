45c3f6645b5901f50a5b9575b4aaf6db3f5b157b
Tinithk <tinithse@gmail.com>
2023-04-17 21:21:07 +0530

    db connect

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
+const mongoose = require('mongoose');
 const dontenv = require('dotenv');
 
 dontenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB, {
+  useNewUrlParser: true
+}).then(() => console.log('DB connection successful !'));
 
 // console.log(process.env);
 
