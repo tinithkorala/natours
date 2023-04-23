800a3f53406f7f0aa083adbfe48e61a016025704
Tinithk <tinithse@gmail.com>
2023-04-23 09:09:37 +0530

    pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -33,14 +33,20 @@ exports.getAllTours = async (req, res) => {
             query = query.select('-__v');
         }
 
+        // 4A) Pagination
+        const page = req.query.page * 1 || 1;
+        const limit = req.query.limit * 1 || 100;
+        const skip = (page - 1) * limit;
+
+        query = query.skip(skip).limit(limit);  
+
+        if(req.query.page) {
+            const numTours = await Tour.countDocuments();
+            if(skip >= numTours) throw new Error('This page dose not exist');
+        }
+
         // EXECUTE QUERY
         const tours = await query;
-
-        // const query = await Tour.find()
-        //     .where('duration')
-        //     .equals(5)
-        //     .where('difficulty')
-        //     .equals('easy');
 
         res.status(200).json({
             status: 'success',
