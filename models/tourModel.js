2b125721e30f98d701f8c7c41243ac2cc827e0ed
Tinithk <tinithse@gmail.com>
2023-04-23 22:51:37 +0530

    adding validation for name

--- models/tourModel.js
+++ models/tourModel.js
@@ -8,7 +8,9 @@ const tourSchema = new mongoose.Schema({
         type: String,
         required: [true, 'A tour must have a name'],
         unique: true,
-        trim: true
+        trim: true,
+        maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+        minlength: [10, 'A tour name must have more or equal then 10 characters']
     },
     slug: String,
     duration: {
