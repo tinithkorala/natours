1ee1b691ef083058c4b31626b42ea84a475659ea
Tinithk <tinithse@gmail.com>
2023-04-23 07:46:39 +0530

    filter done

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,20 +4,28 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
     try {
+        console.log(req.query);
+
         // BUILD QUERY
+        // 1) Filtering
         const queryObj = { ...req.query };
         const excludedFields = ['page', 'sort', 'limit', 'fields'];
         excludedFields.forEach(el => delete queryObj[el]);
 
-        const query = Tour.find(queryObj);
+        // 2) Advanced Filtering
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
 
-        // const tours = await Tour.find()
+        const query = Tour.find(JSON.parse(queryStr));
+
+        // EXECUTE QUERY
+        const tours = await query;
+
+        // const query = await Tour.find()
         //     .where('duration')
         //     .equals(5)
         //     .where('difficulty')
         //     .equals('easy');
-
-        const tours = await query;
 
         res.status(200).json({
             status: 'success',
