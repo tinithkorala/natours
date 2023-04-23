f0bfb983c7ce50e77f788574b9645b8909e20b68
Tinithk <tinithse@gmail.com>
2023-04-23 21:20:52 +0530

    aggregation-middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -55,7 +55,11 @@ const tourSchema = new mongoose.Schema({
         default: Date.now(),
         select: false
     },
-    startDates: [Date]
+    startDates: [Date],
+    secretTour: {
+        type: Boolean,
+        default: false
+    }
 },
 {
     toJSON: { virtuals: true },
@@ -69,6 +73,7 @@ tourSchema.virtual('durationWeeks').get(function() {
     return this.duration / 7;
 });
 
+// DOCUMENT MIDDLEWARE
 tourSchema.pre('save', function(next) {
     this.slug = slugify(this.name, { lower: true });
     next();
@@ -83,6 +88,18 @@ tourSchema.pre('save', function(next) {
 //     console.log(doc);
 //     next();
 // });
+
+// QUERY MIDDLEWARE
+tourSchema.pre(/^find/, function(next) {
+    this.find({ secretTour: { $ne: true } });
+    this.start = Date.now();
+    next();
+});
+
+tourSchema.post(/^find/, function(docs, next) {
+    console.log(`Query took ${Date.now() - this.start} milliseconds`);
+    next();
+});
 
 // Create a model from the schema
 const Tour = mongoose.model('Tour', tourSchema);
