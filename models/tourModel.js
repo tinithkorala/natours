384095959c17a123c72e402abcae03258f338708
Tinithk <tinithse@gmail.com>
2023-04-23 23:08:12 +0530

    demo validation

--- models/tourModel.js
+++ models/tourModel.js
@@ -23,11 +23,17 @@ const tourSchema = new mongoose.Schema({
     },
     difficulty: {
         type: String,
-        required: [true, 'A tour must have a difficulty']
+        required: [true, 'A tour must have a difficulty'],
+        enum: {
+            values: ['easy', 'medium', 'difficult'],
+            message: 'Difficulty is either: easy, medium, difficult'
+        }
     },
     ratingsAverage: {
         type: Number,
-        default: 4.5
+        default: 4.5,
+        min: [1, 'Rating must be above 1.0'],
+        max: [5, 'Rating must be above 5.0'],
     },
     ratingsQuantity: {
         type: Number,
@@ -69,7 +75,7 @@ const tourSchema = new mongoose.Schema({
 }
 );
 
-tourSchema.plugin(uniqueValidator);
+// tourSchema.plugin(uniqueValidator);
 
 tourSchema.virtual('durationWeeks').get(function() {
     return this.duration / 7;
