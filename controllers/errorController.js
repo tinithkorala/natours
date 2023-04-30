60a8e9af4aa45c6bd335c1e8b3edc6ad97dc30e6
Tinithk <tinithse@gmail.com>
2023-04-30 16:23:53 +0530

    handle duplicate error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,17 @@
+const { json } = require("express");
+const AppError = require("./../utils/appError");
+
+const handleCastErrorDB = err => {
+    const message = `Invalid ${err.path}: ${err.value}.`;
+    return new AppError(message, 400);
+}
+
+const handleDuplicateFieldsDB = err => {
+    const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+    const message = `Duplicate field value: ${value}. Please use another value !`;
+    return new AppError(message, 400);
+}
+
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
@@ -24,13 +38,19 @@ const sendErrorProd = (err, res) => {
 }
 
 module.exports = (err, req, res, next) => {
-    // console.log(err.stack);
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
 
     if(process.env.NODE_ENV === 'development') {
         sendErrorDev(err, res);
     }else if(process.env.NODE_ENV === 'production'){
-        sendErrorProd(err, res);
+        // console.log('💥', err);
+        let errmsg = err.message;
+        let error = { ...err, errmsg };
+        // let error = Object.assign({}, err);
+        // let error = JSON.parse(JSON.stringify(err));
+        if(error.name === 'CastError') error = handleCastErrorDB(error);
+        if(error.code === 11000) error = handleDuplicateFieldsDB(error);
+        sendErrorProd(error, res);
     }
 }
