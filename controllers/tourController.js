a29cdd2f248a1a3f5238174030b37310fdcf126b
Tinithk <tinithse@gmail.com>
2023-04-30 12:31:43 +0530

    error handling production or dev env

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,7 @@
 const Tour = require('./../models/tourModel');
 const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
 // const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
 
@@ -32,6 +33,11 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 
 exports.getTour = catchAsync(async (req, res, next) => {
     const tour = await Tour.findById(req.params.id);
+
+    if(!tour) {
+        return next(new AppError('No tour found with that ID', 404));
+    }
+
     res.status(200).json({
         status: 'success',
         data: { 
@@ -55,6 +61,9 @@ exports.updateTour = catchAsync(async (req, res, next) => {
         new: true,
         runValidators: true
     });
+    if(!tour) {
+        return next(new AppError('No tour found with that ID', 404));
+    }
     res.status(200).json({
         status: 'success',
         data: {
@@ -64,7 +73,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 });   
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-    await Tour.findByIdAndDelete(req.params.id) 
+    const tour = await Tour.findByIdAndDelete(req.params.id);
+    if(!tour) {
+        return next(new AppError('No tour found with that ID', 404));
+    }
     res.status(204).json({
         status: 'success',
         data: null
