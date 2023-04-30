59a08d46ae5778e3a487fa8dce4e4ea31371c27f
Tinithk <tinithse@gmail.com>
2023-04-30 17:07:06 +0530

    handle the unhandled promise

--- server.js
+++ server.js
@@ -17,6 +17,14 @@ mongoose.connect(DB, {
 // console.log(process.env);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! 💥 Shutting down... ');
+  server.close(() => {
+    process.exit(1);
+  });
 });
