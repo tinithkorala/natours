6aa3c92d352f1cdd45f75e1815515cf8a5237d0f
Tinithk <tinithse@gmail.com>
2023-05-04 12:14:11 +0530

    developed json token send when create user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,19 @@
+const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-    const newUser = await User.create(req.body);
+    // const newUser = await User.create(req.body);
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfrim: req.body.passwordConfrim,
+    });
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRES_IN});
     res.status(201).json({
-        status: 'success',
+        status: 'success',  
+        token,
         data: {
             user: newUser
         }
