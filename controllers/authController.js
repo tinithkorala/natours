4cc29f2f92437ef28c738fbc0c94d2b3fa6772c6
Tinithk <tinithse@gmail.com>
2023-05-04 13:30:47 +0530

    user login done

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,11 @@
 const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+const signToken = id => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRES_IN})
+}
 
 exports.signup = catchAsync(async (req, res, next) => {
     // const newUser = await User.create(req.body);
@@ -10,12 +15,36 @@ exports.signup = catchAsync(async (req, res, next) => {
         password: req.body.password,
         passwordConfrim: req.body.passwordConfrim,
     });
-    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRES_IN});
+    const token = signToken(newUer._id);
     res.status(201).json({
         status: 'success',  
         token,
         data: {
             user: newUser
         }
+    });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    // 1) Check is email and password is exist
+    if(!email || !password) {
+        return next(new AppError('Please provide email and password!', 400));
+    }
+
+    // 2) Check is user exists && password is correct
+    const user = await User.findOne({ email }).select('+password');
+    // const correct = await user.correctPassword(password, user.password); 
+
+    if(!user || !(await user.correctPassword(password, user.password))) {
+        return next(new AppError('Incorrect email or password', 401)); 
+    }
+
+    // 3) If everything ok, send token to client
+    const token = signToken(user._id);
+    res.status(200).json({
+        status: 'success',
+        token
     });
 });
