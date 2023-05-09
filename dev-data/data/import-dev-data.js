9c8f3fef7182d715c94207716aaf39b9177fdff1
Tinithk <tinithse@gmail.com>
2023-05-09 23:42:04 +0530

    create embadded document

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -15,7 +15,7 @@ mongoose.connect(DB, {
 }).then(() => console.log('DB connection successful !'));
 
 // Read json file
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'));
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 
 // Import data into db
 const importData = async () => {
