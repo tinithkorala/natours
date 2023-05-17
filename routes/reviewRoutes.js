e2c384405b1cad686f058a7bd6061c0f276a39c5
Tinithk <tinithse@gmail.com>
2023-05-17 22:35:40 +0530

    dev

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.route('/')
     .get(reviewController.getAllReviews)
-    .post(authController.protect, authController.restrictTo('user'), reviewController.createReviews);
+    .post(authController.protect, authController.restrictTo('user'), reviewController.createReview);
 
 module.exports = router;
 
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
+const reviewController = require('./../controllers/reviewController');
 
 const router = express.Router();
 
@@ -18,5 +19,8 @@ router.route('/:id')
     .get(tourController.getTour)
     .patch(tourController.updateTour)
     .delete(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.deleteTour);
+
+router.route('/:tourId/reviews')
+    .post(authController.protect, authController.restrictTo('user   '), reviewController.createReview);
 
 module.exports = router;
