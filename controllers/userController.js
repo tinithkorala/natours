8c41f1b0434cc21b4a59ed6efdabcaddf9601906
Tinithk <tinithse@gmail.com>
2023-05-19 00:00:24 +0530

    refactor update and created

--- controllers/userController.js
+++ controllers/userController.js
@@ -64,11 +64,6 @@ exports.getUser = (req, res) => {
         message: 'This route is not defined!'
     });
 }
-exports.updateUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not defined!'
-    });
-}
 
+exports.updateUser = factory.updateOne(User);
 exports.deleteUser = factory.deleteOne(User);
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -6,9 +6,10 @@ const router = express.Router({ mergeParams: true });
 
 router.route('/')
     .get(reviewController.getAllReviews)
-    .post(authController.protect, authController.restrictTo('user'), reviewController.createReview);
+    .post(authController.protect, authController.restrictTo('user'), reviewController.setTourUserIds, reviewController.createReview);
 
 router.route('/:id')
+    .patch(reviewController.updateReview)
     .delete(reviewController.deleteReview)
 
 module.exports = router;
