42e603961dbbb7514bfee2c60d536cd24a151a49
Tinithk <tinithse@gmail.com>
2023-05-28 14:39:44 +0530

    views controller

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
@@ -61,25 +62,8 @@ app.use((req, res, next) => {
     next();
 }); 
 
-app.get('/', (req, res) => {
-    res.status(200).render('base', {
-        tour: 'The forest hicker',
-        user: 'Jonas'
-    });
-});
-
-app.get('/overview', (req, res) => {
-    res.status(200).render('overview', {
-        title: 'All tours'
-    });
-});
-
-app.get('/tour', (req, res) => {
-    res.status(200).render('tour', {
-        title: 'The forest hicker'
-    });
-});
-
+// Routes
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
