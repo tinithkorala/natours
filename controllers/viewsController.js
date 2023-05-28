ec6ae7b9bdb01bb5da7e310f536b4573bb1a3fbe
Tinithk <tinithse@gmail.com>
2023-05-28 18:05:45 +0530

    bug fixed

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -18,7 +18,7 @@ exports.getTour = catchAsync( async (req, res) => {
         fields: 'review rating user'
     });
     res.status(200).render('tour', {
-        title: 'The forest hicker',
+        title: `${tour.name} Tour`,
         tour
     });
 });
