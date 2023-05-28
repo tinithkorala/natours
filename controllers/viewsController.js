963e328fd82d50ac8f477738330d0edcca0ed5a3
Tinithk <tinithse@gmail.com>
2023-05-28 15:23:39 +0530

    overview page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,16 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync( async (req, res, next) => {
+    // Get all data from collection
+    const tours = await Tour.find();
+    // Build Template
+    // Render that template using data from 1
     res.status(200).render('overview', {
-        title: 'All tours'
+        title: 'All Tours',
+        tours
     });
-};
+});
 
 exports.getTour = (req, res) => {
     res.status(200).render('tour', {
