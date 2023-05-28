8a36172486c06fc5f1c0b0b95c812c8cbe96728a
Tinithk <tinithse@gmail.com>
2023-05-28 15:41:23 +0530

    tour display

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -12,8 +12,13 @@ exports.getOverview = catchAsync( async (req, res, next) => {
     });
 });
 
-exports.getTour = (req, res) => {
-    res.status(200).render('tour', {
-        title: 'The forest hicker'
+exports.getTour = catchAsync( async (req, res) => {
+    const tour = await Tour.findOne({slug: req.params.slug}).populate({
+        path: 'reviews',
+        fields: 'review rating user'
     });
-}
+    res.status(200).render('tour', {
+        title: 'The forest hicker',
+        tour
+    });
+});
